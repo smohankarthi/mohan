92cc408ea1b8ab81e5d398d21392e49e45efe318
smohankarthi <30368934+smohankarthi@users.noreply.github.com>
2017-08-23 16:56:42 +0530

    Create vowelsorconsonant.c

---
+++ vowelsorconsonant.c
@@ -0,0 +1,14 @@
+##include<stdio.h>
+main()
+{
+char s;
+scanf("%c",&s);
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||s=='A'||s=='E'||s=='I'||s=='O'||s==)
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+}
